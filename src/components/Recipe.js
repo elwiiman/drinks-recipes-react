598c5a0e0b067d079b54ace08c0c89c4df42be4d
elwiiman <emmanuel.sanro@gmail.com>
2020-05-21 19:44:21 -0500

    modal from material-ui added, Modal context values changed, project completed

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,9 +1,62 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ModalContext } from "../context/ModalContext";
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 450,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 const Recipe = ({ recipe }) => {
+  //modal configuration, comes from material UI
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+
+  const classes = useStyles();
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   //extract from Modal Context
-  const { setIdRecipe } = useContext(ModalContext);
+  const { selectedRecipe, setSelectedRecipe, setIdRecipe } = useContext(
+    ModalContext
+  );
+
+  //Show and format ingridients
+  const showIngridients = (recipe) => {
+    let ingridients = [];
+    for (let i = 1; i < 16; i++) {
+      if (recipe[`strIngredient${i}`]) {
+        ingridients.push(
+          <li>
+            {recipe[`strIngredient${i}`]} {recipe[`strMeasure${i}`]}
+          </li>
+        );
+      }
+    }
+    return ingridients;
+  };
 
   return (
     <div className="col-md-4 mb-3">
@@ -20,10 +73,32 @@ const Recipe = ({ recipe }) => {
             className="btn btn-block btn-primary"
             onClick={() => {
               setIdRecipe(recipe.idDrink);
+              handleOpen();
             }}
           >
             See Recipe
           </button>
+          <Modal
+            open={open}
+            onClose={() => {
+              setIdRecipe(null);
+              handleClose();
+              setSelectedRecipe({});
+            }}
+          >
+            <div style={modalStyle} className={classes.paper}>
+              <h2>{selectedRecipe.strDrink}</h2>
+              <h3 className="mt-4">Instructions</h3>
+              <p>{selectedRecipe.strInstructions}</p>
+              <img
+                className="img-fluid my-4"
+                src={selectedRecipe.strDrinkThumb}
+                alt={selectedRecipe.strDrink}
+              />
+              <h3>Ingridients and Quantities</h3>
+              <ul>{showIngridients(selectedRecipe)}</ul>
+            </div>
+          </Modal>
         </div>
       </div>
     </div>
--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -1,14 +1,28 @@
 import React, { createContext, useEffect, useState } from "react";
 import axios from "axios";
-
 export const ModalContext = createContext();
 
 const ModalProvider = (props) => {
   //state for provider
   const [idRecipe, setIdRecipe] = useState(null);
+  const [selectedRecipe, setSelectedRecipe] = useState({});
+
+  //once we have a recipe, call the API
+  useEffect(() => {
+    const obtainRecipeByApi = async () => {
+      if (!idRecipe) return;
+      const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idRecipe}`;
+      const response = await axios.get(url);
+      setSelectedRecipe(response.data.drinks[0]);
+    };
+
+    obtainRecipeByApi();
+  }, [idRecipe]);
 
   return (
-    <ModalContext.Provider value={{ setIdRecipe }}>
+    <ModalContext.Provider
+      value={{ selectedRecipe, setSelectedRecipe, setIdRecipe }}
+    >
       {props.children}
     </ModalContext.Provider>
   );
